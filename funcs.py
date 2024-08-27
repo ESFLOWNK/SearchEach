a14ede16756749ecfa39ab995c08255826797d0a
ESFLOWNK <otakuflownk@gmail.com>
2024-08-27 17:06:09 -0400

    Support for two characters

--- funcs.py
+++ funcs.py
@@ -1,17 +1,23 @@
 
-def search(sr: str, fl: str):
+def search(sr: str = "",ssr = None, fl: str = ""):
     """
-    Search text between a character or more in the given file
+    Search text between a character or two in the given file
     """
-
-    #if "--and" in sr:
-    #   Add code
-    #   return
-
+    
     #Result
     res = ""
-    
+
     j = -1
+    if ssr:
+        for i in range(len(fl)):
+            if j == -1 and fl[i] == sr:
+                j = i
+            elif not j == -1 and fl[i] == ssr:
+                res += fl[j:i+1] + "\n"
+                j = -1
+
+        return res[:-1]
+    
     for i in range(len(fl)):
         if j == -1 and fl[i] == sr:
             j = i
--- searcheach.py
+++ searcheach.py
@@ -7,28 +7,41 @@ from msvcrt import getch
 
 def main():
 
-    if len(sys.argv) > 3:
+    if len(sys.argv) == 4:
         #fl = file
         #sr = searching
         #flr file result
         fl = sys.argv[1]
         sr = sys.argv[2]
         flr = sys.argv[3]
-
+    
         if sr == "strings":
             sr = '"'
 
+        res = funcs.search(sr=sr,fl=Path(fl).read_text())
+
+    elif len(sys.argv) == 5:
+        #fl = file
+        #sr = searching
+        #ssr second searching
+        #flr file result
+        fl = sys.argv[1]
+        sr = sys.argv[2]
+        ssr = sys.argv[3]
+        flr = sys.argv[4]
+
+        res = funcs.search(sr=sr,ssr=ssr,fl=Path(fl).read_text())
+
     else:
-        print("searcheach [FILE] [CHARACTER] [FILE_RESULT]\n"+
+        print("searcheach FILE CHARACTER [CHARACTER2] FILE_RESULT\n"+
         "\tFILE = The file to check\n"+
-        "\tCHARACTER = The character that the program will search and show its content\n"+
-        "\tFILE_RESULT = The name of the file where the data will be write")
+        "\tCHARACTER = The character used for searching texts between it\n"+
+        "\tCHARACTER2 = (Optional) Texts will be searched between CHARACTER and CHARACTER2\n"+
+        "\tFILE_RESULT = The name of the file where the results will be writen\n\n"+
+        "Press any key to exit...")
         getch()
-        exit()
+        return
 
-    res = funcs.search(sr,Path(fl).read_text())
-
-    #funcs.write(res,flr,sr)
     Path(flr).write_text(res)
 
 if __name__ == "__main__":
