53864f3b63af63f599fc315d84f2b2ffa7326c85
ESFLOWNK <otakuflownk@gmail.com>
2024-08-27 17:08:26 -0400

    Reduce files

--- searcheach.py
+++ searcheach.py
@@ -5,6 +5,34 @@ from pathlib import Path
 import sys
 from msvcrt import getch
 
+def search(sr: str = "",ssr = None, fl: str = ""):
+    """
+    Search text between a character or two in the given file
+    """
+    
+    #Result
+    res = ""
+
+    j = -1
+    if ssr:
+        for i in range(len(fl)):
+            if j == -1 and fl[i] == sr:
+                j = i
+            elif not j == -1 and fl[i] == ssr:
+                res += fl[j:i+1] + "\n"
+                j = -1
+
+        return res[:-1]
+    
+    for i in range(len(fl)):
+        if j == -1 and fl[i] == sr:
+            j = i
+        elif fl[i] == sr:
+            res += fl[j:i+1] + "\n"
+            j = -1
+
+    return res[:-1]
+
 def main():
 
     if len(sys.argv) == 4:
